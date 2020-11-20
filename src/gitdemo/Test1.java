eada09018716fab3391c5fcad4415c4e5171843f
user <sdixit3493>
2020-11-20 10:32:35 +0530

    first commit

---
+++ src/gitdemo/Test1.java
@@ -0,0 +1,14 @@
+package gitdemo;
+
+public class Test1 {
+	
+	public static void main(String[] args) {
+		String hungry="yes";
+		if(hungry=="yes") 
+			System.out.println("eat something...");
+			
+		else
+			System.out.println("Do work....");
+	}
+
+}
